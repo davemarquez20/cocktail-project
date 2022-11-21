628a9dbf889a2e85706cee370643123c362ad242
davemarquez20 <52246960+davemarquez20@users.noreply.github.com>
2022-11-21 20:20:03 +0800

    the first commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h2 className="section-title">about us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+        distinctio, quia expedita culpa repellat ex sint eaque? Minus error
+        aliquam veniam laudantium consequatur ipsam vero eos eum. Quam harum
+        sint ratione? Eaque consequatur recusandae, aut voluptatem delectus
+        excepturi inventore animi voluptatum quasi explicabo dolore in dicta
+        consequuntur repellat omnis voluptas.
+      </p>
+    </section>
+  );
+};
+
+export default About;
